71d792c64e8d8eb1e940ff68f4a19fae544bc267
Jigor <sloy425@gmail.com>
2025-09-28 16:05:26 +0300

    init1

--- main-app/pages/urls.py
+++ main-app/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, CatalogPage
+
 
 urlpatterns = [
     path('', HomePage, name='home'),
+    path('catalog/', CatalogPage, name='catalog'),
 ]
--- main-app/pages/views.py
+++ main-app/pages/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def HomePage(request):
-    return render(request, 'home.html')
+    return render(request, 'pages/home.html')
+
+
+def CatalogPage(request):
+    return render(request, 'pages/catalog.html')
+
+
+def Custom404(request, exception):
+            return render(request, '404.html', status=404)
